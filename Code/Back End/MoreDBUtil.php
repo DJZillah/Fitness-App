1660ddcec0767231057e96e05d86cf2061e2f002
DJZillah <161680944+DJZillah@users.noreply.github.com>
2025-03-19 15:00:46 -0500

    Add files via upload
    DB Connection Utility file with comments explaining its usage, remember to use namespaces.

--- Code/Back End/MoreDBUtil.php
+++ Code/Back End/MoreDBUtil.php
@@ -1,14 +1,17 @@
 <?php
+namespace Fitify;
+
 $servername = "fitify-db.ctq460w22gbq.us-east-2.rds.amazonaws.com";
 $username = "root"; //I believe in root supremacy
 $password = "fitify123";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+//Create connection
+$conn = new \mysqli($servername, $username, $password); // \ ensures it uses global php class
 
-// Check connection
+//Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully"; 
+//use echo for debugging 
 ?>
