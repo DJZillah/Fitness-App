f4c8efe7e0ee0e709bfea98ae29de3a6a2451da6
fernando1c <164610123+fernando1c@users.noreply.github.com>
2025-04-16 17:52:40 -0500

    Update fitifyDB.sql

--- Code/Database/fitifyDB.sql
+++ Code/Database/fitifyDB.sql
@@ -34,8 +34,9 @@ CREATE TABLE `Simple_Cal_Log` (
   `LogID` int NOT NULL AUTO_INCREMENT,
   `TotalCal` int NOT NULL,
   `LogDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int unsigned DEFAULT NULL,
   PRIMARY KEY (`LogID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +45,7 @@ CREATE TABLE `Simple_Cal_Log` (
 
 LOCK TABLES `Simple_Cal_Log` WRITE;
 /*!40000 ALTER TABLE `Simple_Cal_Log` DISABLE KEYS */;
-INSERT INTO `Simple_Cal_Log` VALUES (1,890,'2025-03-21 15:24:28'),(2,1074,'2025-03-21 15:46:10');
+INSERT INTO `Simple_Cal_Log` VALUES (1,1436,'2025-04-06 15:51:29',8),(2,1850,'2025-04-07 17:36:14',8),(3,26,'2025-04-07 17:46:36',8),(4,26,'2025-04-07 17:51:02',8),(5,50,'2025-04-08 18:38:58',8),(6,429,'2025-04-08 19:20:26',8),(7,910,'2025-04-16 22:23:53',6);
 /*!40000 ALTER TABLE `Simple_Cal_Log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,8 +89,11 @@ CREATE TABLE `bmi_records` (
   `bmi` float NOT NULL,
   `category` varchar(50) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user` (`user_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,39 +102,38 @@ CREATE TABLE `bmi_records` (
 
 LOCK TABLES `bmi_records` WRITE;
 /*!40000 ALTER TABLE `bmi_records` DISABLE KEYS */;
-INSERT INTO `bmi_records` VALUES (1,'goob',67,180,28.1889,'Overweight','2025-03-25 02:53:33'),(2,'goob',67,180,28.1889,'Overweight','2025-03-25 03:01:47');
+INSERT INTO `bmi_records` VALUES (1,'goob',67,180,28.1889,'Overweight','2025-03-25 02:53:33',NULL),(2,'goob',67,180,28.1889,'Overweight','2025-03-25 03:01:47',NULL),(3,'Chocolate',68,250,38.0082,'Obese','2025-03-27 00:11:06',NULL),(4,'David',70,202,28.9808,'Overweight','2025-04-08 23:25:18',NULL),(9,'David',70,202,28.9808,'Overweight','2025-04-09 00:19:57',NULL),(15,'Parker Ayala',70,135,19.3684,'Normal weight','2025-04-16 20:59:59',6),(16,'fernando1c',69,170,25.1019,'Overweight','2025-04-16 22:09:50',NULL);
 /*!40000 ALTER TABLE `bmi_records` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `calories_log`
+-- Table structure for table `burned_calories_log`
 --
 
-DROP TABLE IF EXISTS `calories_log`;
+DROP TABLE IF EXISTS `burned_calories_log`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `calories_log` (
+CREATE TABLE `burned_calories_log` (
   `log_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int DEFAULT NULL,
-  `activity_id` int NOT NULL,
-  `total_calories` decimal(6,2) NOT NULL,
-  `log_date` date NOT NULL,
-  `duration_minutes` int NOT NULL,
+  `activity_name` varchar(100) DEFAULT NULL,
+  `calories_burned` int DEFAULT NULL,
+  `log_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `duration_minutes` int DEFAULT NULL,
   PRIMARY KEY (`log_id`),
   KEY `user_id` (`user_id`),
-  KEY `activity_id` (`activity_id`),
-  CONSTRAINT `calories_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  CONSTRAINT `calories_log_ibfk_2` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`activity_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `burned_calories_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `calories_log`
+-- Dumping data for table `burned_calories_log`
 --
 
-LOCK TABLES `calories_log` WRITE;
-/*!40000 ALTER TABLE `calories_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `calories_log` ENABLE KEYS */;
+LOCK TABLES `burned_calories_log` WRITE;
+/*!40000 ALTER TABLE `burned_calories_log` DISABLE KEYS */;
+INSERT INTO `burned_calories_log` VALUES (2,9,'Running',823,'2025-04-16 22:00:40',30);
+/*!40000 ALTER TABLE `burned_calories_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -224,6 +227,39 @@ LOCK TABLES `meals` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `milestones`
+--
+
+DROP TABLE IF EXISTS `milestones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `milestones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `milestone_name` varchar(255) NOT NULL,
+  `achieved_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `exercise_id` int DEFAULT NULL,
+  `max_weight` int DEFAULT NULL,
+  `reps` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `exercise_id` (`exercise_id`),
+  CONSTRAINT `milestones_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  CONSTRAINT `milestones_ibfk_2` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `milestones`
+--
+
+LOCK TABLES `milestones` WRITE;
+/*!40000 ALTER TABLE `milestones` DISABLE KEYS */;
+INSERT INTO `milestones` VALUES (9,7,'lifted a lot','2025-04-09 02:03:47',NULL,NULL,NULL),(10,7,'','2025-04-16 02:49:12',1,50,10);
+/*!40000 ALTER TABLE `milestones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `training_plans`
 --
 
@@ -274,7 +310,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -283,6 +319,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (6,'Parker Ayala','Ayalp126@gmail.com','$2y$10$gKWkSoh7iXtNzaYupeOAQOnab5goXUPDcmTzn1jxu7CCFe7f6dyTa',22,140.00,69.00,'2025-03-27 00:19:24'),(7,'goob','goob@goob.com','$2y$10$HAbvniGQ..HYn/7sQNVzuuJpoHFHbuF.3Q7qBnUfvjsg4S86.y/dS',26,180.00,67.00,'2025-04-02 02:42:38'),(8,'David','djshanesy@gmail.com','$2y$10$tzMEl5FXxDT9DeNTKB7F8uyy2auC6iiZTc/P3FFEWX0BA5LjqaFiu',23,205.00,70.00,'2025-04-06 18:59:35'),(9,'fernando1c','fernandonoveron@gmail.com','$2y$10$c/5zKvrj0jlSLiQinyVk4ul14haCHqtM.UBDg4ZCj1sTvlC3LBo52',19,160.00,69.00,'2025-04-09 22:07:11');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -297,8 +334,11 @@ CREATE TABLE `weight_log` (
   `log_id` int NOT NULL AUTO_INCREMENT,
   `weight` decimal(5,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`log_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `weight_log_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -307,7 +347,7 @@ CREATE TABLE `weight_log` (
 
 LOCK TABLES `weight_log` WRITE;
 /*!40000 ALTER TABLE `weight_log` DISABLE KEYS */;
-INSERT INTO `weight_log` VALUES (2,150.00,'2025-03-25 22:07:53'),(5,180.00,'2025-03-25 22:31:07');
+INSERT INTO `weight_log` VALUES (13,135.00,'2025-04-16 20:32:57',6),(14,170.00,'2025-04-16 22:10:20',9);
 /*!40000 ALTER TABLE `weight_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -331,7 +371,7 @@ CREATE TABLE `workout_logs` (
   PRIMARY KEY (`workout_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `workout_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -340,6 +380,7 @@ CREATE TABLE `workout_logs` (
 
 LOCK TABLES `workout_logs` WRITE;
 /*!40000 ALTER TABLE `workout_logs` DISABLE KEYS */;
+INSERT INTO `workout_logs` VALUES (22,6,'Bicep Curls',20,300.00,25.00,12,3,'2025-04-16 20:29:49');
 /*!40000 ALTER TABLE `workout_logs` ENABLE KEYS */;
 UNLOCK TABLES;
 SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
@@ -353,4 +394,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-26 18:54:08
+-- Dump completed on 2025-04-16 17:49:38
